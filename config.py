ef59589ac46a9730cd2e2a29e31fab6a60fc2061
Skittle <skittleandbeluga123@gmail.com>
2026-03-14 04:14:44 +0530

    fix config env vars

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("8774860311:AAEoWrnPB_9n3Xfn9ikMUTDcC0sjGsBARgE")
-OWNER_ID = int(os.getenv("8379924791"))
-ADMIN_GROUP_ID = int(os.getenv("-1003755315134"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID"))
